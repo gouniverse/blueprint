708ac799ca510ee69a5046de706eb5175601423e
Milan Lesichkov <lesichkovm@gmail.com>
2023-10-08 16:44:01 +0000

    Updates

--- internal/helpers/ToFlash.go
+++ internal/helpers/ToFlash.go
@@ -1,23 +1,58 @@
 package helpers
 
 import (
-	"fmt"
 	"net/http"
 	"project/config"
 	"project/internal/links"
+	"strings"
 
 	"github.com/gouniverse/uid"
 )
 
-// GuestFlash redirects the user to a flash page
-func ToFlash(w http.ResponseWriter, r *http.Request, messageType string, message string, url string, seconds int) string {
+const FlashError = "error"
+const FlashSuccess = "success"
+const FlashInfo = "info"
+const FlashWarning = "warning"
+
+func IsFlashRoute(r *http.Request) bool {
+	return strings.Contains(r.URL.Path, "/flash")
+}
+
+// ToFlashURL return a flash message URL
+func ToFlashURL(messageType string, message string, url string, seconds int) string {
 	id := uid.HumanUid()
-	config.Cms.CacheStore.Set(id+"_flash_message", message, int64(seconds)+10)
-	config.Cms.CacheStore.Set(id+"_flash_type", messageType, int64(seconds)+10)
-	config.Cms.CacheStore.Set(id+"_flash_url", url, int64(seconds)+10)
-	config.Cms.CacheStore.Set(id+"_flash_time", fmt.Sprint(seconds), int64(seconds)+10)
-	http.Redirect(w, r, links.NewWebsiteLinks().Flash(map[string]string{
+	config.Cms.CacheStore.SetJSON(id+"_flash_message", map[string]any{
+		"type":    messageType,
+		"message": message,
+		"url":     url,
+		"time":    seconds,
+	}, int64(seconds)+10)
+
+	return links.NewWebsiteLinks().Flash(map[string]string{
 		"message_id": id,
-	}), http.StatusSeeOther)
+	})
+}
+
+// ToFlash redirects the user to a flash page
+func ToFlash(w http.ResponseWriter, r *http.Request, messageType string, message string, url string, seconds int) string {
+	flashUrl := ToFlashURL(messageType, message, url, seconds)
+
+	http.Redirect(w, r, flashUrl, http.StatusSeeOther)
 	return ""
+}
+
+func ToFlashError(w http.ResponseWriter, r *http.Request, message string, url string, seconds int) string {
+	return ToFlash(w, r, FlashError, message, url, seconds)
+}
+
+func ToFlashInfo(w http.ResponseWriter, r *http.Request, message string, url string, seconds int) string {
+	return ToFlash(w, r, FlashInfo, message, url, seconds)
+}
+
+func ToFlashSuccess(w http.ResponseWriter, r *http.Request, message string, url string, seconds int) string {
+	return ToFlash(w, r, FlashSuccess, message, url, seconds)
+}
+
+func ToFlashWarning(w http.ResponseWriter, r *http.Request, message string, url string, seconds int) string {
+	return ToFlash(w, r, FlashWarning, message, url, seconds)
 }
