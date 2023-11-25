f114071fe2450492bcc00c94f4d4df6dd19e5f49
Sinevia <info@sinevia.com>
2023-11-25 07:37:37 +0000

    Updated auth middleware

--- internal/middlewares/NewUserMiddleware.go
+++ internal/middlewares/NewUserMiddleware.go
@@ -2,6 +2,8 @@ package middlewares
 
 import (
 	"net/http"
+	"project/internal/helpers"
+	"project/internal/links"
 
 	"github.com/gouniverse/router"
 )
@@ -11,7 +13,23 @@ func NewUserMiddleware() router.Middleware {
 		Name: "User Middleware",
 		Handler: func(next http.Handler) http.Handler {
 			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				// User validation logic here
+				// User validation logic here. Change with your own
+
+				authUser := helpers.GetAuthUser(r)
+
+				if authUser == nil {
+					returnURL := links.URL(r.URL.Path, map[string]string{})
+					loginURL := links.NewAuthLinks().Login(returnURL)
+					helpers.ToFlash(w, r, "error", "Only users can access this page", loginURL, 15)
+					return
+				}
+
+				if !authUser.IsActive() {
+					homeURL := links.NewWebsiteLinks().Home()
+					helpers.ToFlash(w, r, "error", "User account not active", homeURL, 15)
+					return
+				}
+
 				next.ServeHTTP(w, r)
 			})
 		},
